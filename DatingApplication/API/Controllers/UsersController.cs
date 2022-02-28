1ca44f732dd514e54b74180ba8a7ee57cebf0a08
fLanc3GV <46271420+Flance-s@users.noreply.github.com>
2022-02-27 21:22:00 -0600

    End of section 4

--- DatingApplication/API/Controllers/UsersController.cs
+++ DatingApplication/API/Controllers/UsersController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
@@ -19,10 +20,12 @@ namespace API.Controllers
         }
 
         [HttpGet]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
             return await _context.Users.ToListAsync();
         }
 
+        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUsers(int id){
             return await _context.Users.FindAsync(id);
---
+++ DatingApplication/API/DTOs/LoginDto.cs
@@ -0,0 +1,8 @@
+namespace API.DTOs;
+
+public class LoginDto
+{
+    public String Username { get; set; }
+
+    public String Password { get; set; }
+}
